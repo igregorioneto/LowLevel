cc2afae78aecd5f5c056cf4d348222bf64be0079
igregorioneto <igregoriodoneto@outlook.com>
2024-08-15 17:48:46 -0300

    feat: library aritmetica

---
+++ library/aritmetica/aritmetica.c
@@ -0,0 +1,13 @@
+#include "aritmetica.h"
+int soma(int x, int y) {
+    return x + y;
+}
+int subtracao(int x, int y) {
+    return x - y;
+}
+int produto(int x, int y) {
+    return x * y;
+}
+int divisao(int x, int y) {
+    return x / y;
+}
---
+++ library/aritmetica/aritmetica.h
@@ -0,0 +1,4 @@
+int soma(int x, int y);
+int subtracao(int x, int y);
+int produto(int x, int y);
+int divisao(int x, int y);
