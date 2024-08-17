37cfd904810bdfd71242d21120ce8844f873b35f
igregorioneto <igregoriodoneto@outlook.com>
2024-08-17 16:30:37 -0300

    feat: Adjusting values vim

--- library/aritmetica/aritmetica.c
+++ library/aritmetica/aritmetica.c
@@ -1,4 +1,6 @@
 #include "aritmetica.h"
+#include <math.h>
+
 int soma(int x, int y) {
     return x + y;
 }
@@ -10,4 +12,7 @@ int produto(int x, int y) {
 }
 int divisao(int x, int y) {
     return x / y;
+}
+int potencia(int x, int y) {
+    return pow(x, y);
 }
