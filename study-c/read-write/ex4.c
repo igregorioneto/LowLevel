41859a2f316778d5f9fd9d02ac1a6cfa51727976
igregorioneto <igregoriodoneto@outlook.com>
2024-08-19 21:04:01 -0300

    feat: creating 5 exercices of read and write with c language

---
+++ study-c/read-write/ex4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+  int x;
+  scanf("%d", &x);
+  printf("%f", x);
+  return 0;
+}
---
+++ study-c/read-write/ex5.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+  float x;
+  scanf("%f", &x);
+  printf("%d", x);
+  return 0;
+}
