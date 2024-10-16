93bf3e0130d4c4da6742a5b04f7308f1852ebb46
igregorioneto <igregoriodoneto@outlook.com>
2024-10-16 19:39:59 -0300

    feat: creating exercise count flowers in c program

---
+++ study-c/code_wars_exercises/flowers.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+
+bool lovefunc(int flower1, int flower2) {
+  int moduleFlower1 = (flower1 % 2), moduleFlower2 = (flower2 % 2);
+	return (moduleFlower1 == 0 && moduleFlower2 == 1) || (moduleFlower1 == 1 && moduleFlower2 == 0);
+}
+
+int main()
+{
+  printf("Flower 1 = 10, Flower 2 = 7, %d\n", lovefunc(10, 7));
+  return 0;
+}
