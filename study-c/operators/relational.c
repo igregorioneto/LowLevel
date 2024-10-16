2c9a94da5278151e851cef47c8715779c1ba205f
igregorioneto <igregoriodoneto@outlook.com>
2024-10-16 16:10:14 -0300

    feat: Operations with programming in C

---
+++ study-c/operators/relational.c
@@ -0,0 +1,17 @@
+// C program to illustrate the relacional operators
+#include <stdio.h>
+
+int main()
+{
+  int a = 25, b = 5;
+
+  // using operators and printing results
+  printf("a < b : %d\n", a < b);
+  printf("a > b : %d\n", a > b);
+  printf("a <= b : %d\n", a <= b);
+  printf("a >= b : %d\n", a >= b);
+  printf("a == b : %d\n", a == b);
+  printf("a != b : %d\n", a != b);
+
+  return 0;
+}
